342c3a31a78ce2c0811b82d4420f66d38e23e138
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2025-12-16 00:29:41 -0600

    Se realizaron los ejercicios 03 y 05

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,21 +4,64 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
-
+//Comentario en una sola linea
 // 2. Escribe un comentario en varias líneas
-
+/*Comentario 
+en 
+varias 
+lineas*/
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+const a ="Cadena"//string
+const b = 1 //Numero
+const c = 1.2//Numero decimal
+const d = false//booleano
+const e = 123n // big integer
+const f = null // null
+let g = Symbol("g")
+let undefinedValue 
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(`5. tipode de todas las variables
+    -`)
+    console.log(`la variable ${a}es ${typeof(a)}`)
+    console.log(`la variable ${b}es ${typeof(b)}`)
+    console.log(`la variable ${c}es ${typeof(c)}`)
+    console.log(`la variable ${d}es ${typeof(d)}`)
+    console.log(`la variable ${e}es ${typeof(e)}`)
+    console.log(`la variable ${f}es ${typeof(f)}`)
+    console.log(`la variable g es ${typeof g}`)
+
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
-
+const h ="Cadena"//string
+const i = 1 //Numero
+const j = 1.2//Numero decimal
+const k = false//booleano
+const l = 123n // big integer
+const m = null // null
+// const n = Symbol("g")
+// const undefinedValue 
 // 9. A continuación, modifica los valores de las constantes
-
+// const h ="Cadena"//string
+// const i = 1 //Numero
+// const j = 1.2//Numero decimal
+// const k = false//booleano
+// const l = 123n // big integer
+// const m = null // null
+// let n = Symbol("g")
+// let undefinedValue 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -4,22 +4,41 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
 
 // 1. Crea una variable para cada operación aritmética
+let a = 1+2
+let b = 2-1
+let c = 2*3
+let d = 10/5
+let e = 2**4
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
-
+console.log(4<5)
+console.log(5==5)
+console.log(6>5)
+console.log(0!=5)
+console.log(!(6==5))
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
-
+console.log(6<5)
+console.log(9==5)
+console.log(2>5)
+console.log(5!=5)
+console.log(!(5==5))
 // 5. Utiliza el operador lógico and
-
+console.log(5==5 &&3>2)
 // 6. Utiliza el operador lógico or
-
+console.log(5>9 || 6>8)
 // 7. Combina ambos operadores lógicos
-
+console.log(3!=="5" && 4==3 || 3==2)
 // 8. Añade alguna negación
-
+console.log(3!=="5" && 4==3 || 3==2 ||!(true))
 // 9. Utiliza el operador ternario
-
+const edad = 24
+edad > 24 ? console.log("Ya estas grandecito") : console.log("Tas chiquito")
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+let nota1 = 9;
+let nota2 = 8;
+let nota3 = 3;
+
+(nota1 + nota2 + nota3) / 3 < 6 ? console.log("Reprobaste, que burro") : console.log("Felicidades, aprobaste")
