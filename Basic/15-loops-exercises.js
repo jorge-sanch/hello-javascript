f6985c0f21821fdcd88d304aee881152edd97146
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2026-01-02 00:00:13 -0600

    Se resolvieron los ejercicios 15 y 17 de loops y de functions

--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -6,21 +6,94 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
+let num = 0
+while (num <= 20 ) {
+    console.log(num)
+    num++;
+}
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
+let suma = 0;
+for(let i = 1; i <=100 ; i++){
+    suma = suma + i
+}
+console.log(suma)
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+let i = 0;
+suma = 0;
+while(i<=50){
+    if (i%2 == 0){
+        suma = suma + i
+    }
+    i++
+}
+console.log(suma)
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
-
+let nombres = ["Jorge","Luis","Sanchez","Jimenez"]
+for(i = 0; i < nombres.length; i++){
+    console.log(`El nombre es: ${nombres[i]}`)
+}
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
-
+let cadena = "faciles"
+const vocales = ["a", "e", "i", "o", "u"]
+let numVocales = 0
+for(i = 0; i < cadena.length; i++){
+    for(let j = 0; j < vocales.length; j++){
+        if(cadena[i]==vocales[j])
+            numVocales +=1
+    }
+}
+console.log(`El numero de vocales de la cadena es:${numVocales}`)
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+let numerosArray = [2,4,6]
+i = 0;
+let multiplicacion = 1
+while(i<numerosArray.length){
 
+    multiplicacion = multiplicacion * numerosArray[i]
+    i++
+} 
+console.log("la multiplicacion de la cadena de numeros es:", multiplicacion)
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
-
+for(i = 0; i<=10; i++){
+    console.log(`5 * ${i} = ${ 5 * i}`)
+}
 // 8. Usa un bucle para invertir una cadena de texto
-
+cadena = "Cadena a invertir"
+let cadenaInvertida = ""
+for(i = cadena.length -1 ; i > -1 ; i--){
+    cadenaInvertida += cadena[i]
+}
+console.log(cadenaInvertida)
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
-
+let a = 0;
+let b = 1;
+let c = 0;
+for( i = 0; i <= 10; i ++){
+    console.log(a)
+    c = a+b;
+    a = b
+    b = c
+    }
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+numerosArray = [3,5,16,4,31,20,4,16]
+let numerosMayoresde10 = []
+for(i =  0; i <= numerosArray.length; i++){
+    if(numerosArray[i]>=10){
+        numerosMayoresde10.push(numerosArray[i])
+    }
+}
+console.log(numerosMayoresde10)
+
+// 10. Dado un array de números, usa un bucle que sume solo los numeros mayoreso  iguales de 10
+
+numerosArray = [3,5,16,4,31]
+let sumaNumerosMayoresA10 = 0
+for( i =  0 ; i <= numerosArray.length ; i++){
+    if (numerosArray[i] >= 10){
+        sumaNumerosMayoresA10 =  sumaNumerosMayoresA10 + numerosArray[i]
+    }
+}
+console.log(sumaNumerosMayoresA10)
--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -6,21 +6,104 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 // NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
 
 // 1. Crea una función que reciba dos números y devuelva su suma
-
+function suma(a,b){
+    return a+b;
+}
+console.log(suma(8,5))
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
-
+let arrayNumeros  = [1,2,3,4,59,6,71,8,9,10]
+function numeroMayor(a){
+    let mayor
+    let comparar = a[0]
+    for( let i = 0 ; i <= a.length ; i++)
+        if(comparar < a[i]){
+            comparar = a[i]
+        }
+        return comparar
+}
+console.log(numeroMayor(arrayNumeros))
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
-
+function contadorDeVocales(texto){
+    let vocales = 0
+    for(let i = 0 ; i< texto.length; i++){
+        if (texto[i] =="a"||texto[i] =="e"||texto[i] =="i"||texto[i] =="o"||texto[i] =="u"){
+            vocales ++
+        }
+    }
+    return vocales  
+}
+console.log(contadorDeVocales("pepe pecas"))
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
-
+let arraysDeStrings = ["string 1", "string 2", "String 3"]
+function arrayMayusculas(arrays){
+    let newArraya = []
+    for(let i = 0; i < arrays.length ; i++){
+        newArraya.push(arrays[i].toUpperCase())
+    }
+    return newArraya
+}
+console.log(arrayMayusculas(arraysDeStrings));
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+function primo(numero){
+    let esPrimo = false
+    for (let  i = 0 ; i < esPrimo ; i --){
+    }
+}
 
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
-
+let mascotas = ["perro", "gato", "kiwi", "piedra", "conejo",2]
+let frutas = ["manzana", "uva", "kiwi", "piedra", "guayaba",2]
+function comparacionValores(array1, array2){
+    let newArray = []
+for(let i = 0; i <mascotas.length; i++){
+    for(let j = 0; j< frutas.length; j++){
+        if (frutas[j] == mascotas[i]){
+            newArray.push(frutas[j])
+            }
+        }
+    }
+    return newArray
+}
+console.log(comparacionValores(mascotas,frutas))
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+let numeros = [1,2,3,4,6,5,10,9,7,7]
+function sumarNumerosPares(arrayDeNumeros){
+    let sumaDeNumerosPares = 0;
+    for( let i = 0 ; i < arrayDeNumeros.length ; i ++){
+        if (arrayDeNumeros[i] % 2 == 0) {
+            sumaDeNumerosPares = sumaDeNumerosPares + arrayDeNumeros[i]
+        }
+    }
+    return sumaDeNumerosPares
+}
+console.log(sumarNumerosPares(numeros))
 
 // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
-
+numeros = [1,2,3,4,5]
+function numerosCuadrado(arrayNumeros){
+    let newArray = []
+    for(let i = 0; i < arrayNumeros.length ; i++)
+        newArray.push(arrayNumeros[i]* arrayNumeros[i])
+    return newArray;
+}
+console.log(numerosCuadrado(numeros))
 // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
 
+function invertirCadena(cadena){
+    let newCadena = ""
+    for(let i = cadena.length -1; i >= 0 ; i--){
+        newCadena += cadena[i]
+    }
+    return newCadena;
+}
+console.log(invertirCadena("esto se tiene que invertir"))
 // 10. Crea una función que calcule el factorial de un número dado
+function factoriales(numero) {
+    let resultado  = numero
+    while (numero>2){
+        resultado = (numero-1) * resultado
+        numero--
+    }
+    return resultado
+}
+console.log(factoriales(10))
